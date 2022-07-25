151d5d465001eb9ba11d82ff12320a3c89f1a982
Sir丶雨轩 <1718018032@qq.com>
2022-07-25 14:40:36 +0800

    修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 module.exports = defineConfig({
+  configureWebpack:{
+    plugins:[
+      new FileManagerPlugin({
+        events:{
+          onEnd: {
+            delete: [
+              './dist/dist.zip',
+            ],
+            archive: [
+              {source: './dist', destination: './dist/dist.zip'},
+            ]
+          }
+        }
+      })
+    ]
+  },
   transpileDependencies: true
 })
